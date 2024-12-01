75bcce98fe2d9a9a14182eb75ea6e1de08ac32d0
Tommaso Dong <dongtommaso03@gmail.com>
2024-12-01 11:21:53 +0100

    1/12/24

--- Tester/Assets/Scripts/Timer.cs
+++ Tester/Assets/Scripts/Timer.cs
@@ -8,30 +8,31 @@ public class Timer : MonoBehaviour
     [SerializeField] float levelTimer;
     [SerializeField] AudioSource audioSource;
     bool gameOver = false;
+    float trueLevelTimer = 0;
     
     void Start()
     {
-        
+        trueLevelTimer = levelTimer + 4.35f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(levelTimer > 0.1f)
+        if(trueLevelTimer > 0.1f)
         {
-            levelTimer -= Time.deltaTime;
+            trueLevelTimer -= Time.deltaTime;
         }
         else if(!gameOver)
         {
             gameOver = true;
-            levelTimer = 0;
+            trueLevelTimer = 0;
             Time.timeScale = 0;
             StopAllAudio();
             audioSource.Play();
             gameOverScreen.SetActive(true);
         }
-        int minutes = Mathf.FloorToInt(levelTimer / 60);
-        int seconds = Mathf.FloorToInt(levelTimer % 60);
+        int minutes = Mathf.FloorToInt(trueLevelTimer / 60);
+        int seconds = Mathf.FloorToInt(trueLevelTimer % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
     private void StopAllAudio() {
